0886686eaf848c5e7374f832bdb20f93cc1edd7c
treyferAI <treyjshaffer@gmail.com>
2017-10-11 22:50:12 +0000

    Game loop framework

--- public_html/game.js
+++ public_html/game.js
@@ -3,6 +3,10 @@ window.onload=function()
     start();
 }
 
+var population;
+var growthRate;
+var running;
+
 function start() {
     console.log("Pre-allocation START");
 
@@ -12,3 +16,35 @@ function start() {
     canvas.height = 500;
     
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function init() { 
+    population = 1;
+    growthRate = 2.0; 
+    running = true;
+    run();
+}
+
+function run() {
+    while(running) {
+        update();
+        draw();
+        
+    }
+}
+
+function update() {
+    updatePopulation();
+}
+
+function draw() {
+
+}
+
+function updatePopulation() {
+
+}
+
