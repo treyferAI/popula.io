aa40daa9a3181fca8c44656a7d671c9d9274d42f
LegacyView <collin.scribner@aol.com>
2017-10-11 22:30:19 -0600

    Initial commit part 2
    Puuuuuuuuuuuushhhhh it

---
+++ public_html/game.js
@@ -0,0 +1,14 @@
+window.onload=function()
+{
+    start();
+}
+
+function start() {
+    console.log("Pre-allocation START");
+
+    var canvas = document.getElementById("myCanvas");
+
+    canvas.width = 500;
+    canvas.height = 500;
+    
+}
